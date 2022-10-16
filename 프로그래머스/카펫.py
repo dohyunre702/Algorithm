644cedda7325277df249a32b8496348d5d65128c
dohyu <dohyunre702@gmail.com>
2022-10-16 21:17:33 +0900

    완전탐색

--- 프로그래머스/카펫.py
+++ 프로그래머스/카펫.py
@@ -1,32 +1,21 @@
-from itertools import combinations_with_replacement
+# 새로운 방식으로 접근
+# 노란색 가로 = x, 노란색 세로 = y
 
-# 구현이 되지 않음..
+# 전체 면적 = (x+2)(y+2)
+# 노란색 = xy
+# 갈색 면적 = 전체 면적 - 노란색 면적 = (x*2)+(y*2)+4
 
-# 접근방식 : 약수 개수 구하기
-def solution(brown, yellow):  # 10, 2
-    prime = brown + yellow
-    primearr = []
-    for i in range(1, prime + 1):
-        if (prime % i == 0):
-            primearr.append(i)
+def solution(brown, yellow):
+    answer = []
+    x = 0  # 가로
+    y = 0  # 세로
+    for i in range(1, yellow + 1):
+        if yellow % i == 0:  # i가 yellow로 곱해지면
+            x = i
+            y = yellow // i
+            if x * 2 + y * 2 + 4 == brown:
+                answer.append(x + 2)
+                answer.append(y + 2)
+                return sorted(answer, reverse=True)  # 내림차순 정렬
 
-    arr = list(combinations_with_replacement(primearr, 2))  # 중복조합으로 2개 뽑기
-
-    for i in range(len(arr)):  # 각 조합의 원소 추출
-        # print(arr[i]) #왜 튜플로 인쇄되는거지?
-        x = arr[i][0]
-        y = arr[i][1]
-
-        if x < y:  # 가로가 세로보다 작으면 해당 리스트 제거
-            del arr[i]
-        if x * y != prime:  # x,y의 곱이 prime이 아니면 제거
-            del arr[i]
-        if (x - 2) * (y - 2) != yellow:
-            del arr[i]
-        if (2 * x) + (2 * y) - 4 != brown:
-            del arr[i]
-        if x <= 2 or y <= 2:
-            del arr[i]
-
-    return arr
-    # IndexError: list index out of range > arr 길이가 바뀌어서 나는 오류같음
+    return answer

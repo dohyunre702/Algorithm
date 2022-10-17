6729affcd6f31e38c0e9bccc50d31cd0850f45ec
dohyu <dohyunre702@gmail.com>
2022-10-17 09:08:45 +0900

    일일알고리즘_1017

---
+++ Likelion/Oct17/Star1.java
@@ -0,0 +1,11 @@
+package Likelion.Oct17;
+
+public class Star1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+
+    }
+}

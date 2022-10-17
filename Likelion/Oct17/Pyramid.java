43cde1aa9af7435034e4a5698d174853c67fa6ce
dohyu <dohyunre702@gmail.com>
2022-10-17 19:16:08 +0900

    pyramid_시도4. 공백 없는 피라미드

--- Likelion/Oct17/Pyramid.java
+++ Likelion/Oct17/Pyramid.java
@@ -3,20 +3,20 @@ package Likelion.Oct17;
 public class Pyramid {
     /*
        *
-      * *
-     * * *
-    * * * *
+      ***
+     *****
+    *******
     //직각삼각형은 * 개수에만 신경쓰면 됨
     //피라미드는 * 위치에도 신경을 써야 함
      */
     public void welcomeToEgypt(int n) {
         for (int i = 0; i < n; i++) {
             //j는 공백
-            for (int j = 4-1-i; j > 0; j--) {
+            for (int j = n-1-i; j > 0; j--) {
                 System.out.printf(" ");
             }
-            for (int k = 0; k < i+1; k++) {
-                System.out.printf("* ");
+            for (int k = 0; k < 2*i+1; k++) {
+                System.out.printf("*");
             }
             System.out.println();
         }

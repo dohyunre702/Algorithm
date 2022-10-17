2a010ff4c99983c51f22c74f0a03f540eeb37fff
dohyu <dohyunre702@gmail.com>
2022-10-17 09:35:16 +0900

    star 조건수정_for문 하나 더, 메소드 추가_답안

--- Likelion/Oct17/Star1.java
+++ Likelion/Oct17/Star1.java
@@ -11,7 +11,7 @@ package Likelion.Oct17;
 import java.util.Scanner;
 
 public class Star1 {
-    public void nightSky(String n) {
+    public void nightSky(int n) {
         for (int i = 0; i < 4; i++) {
             // for문을 하나 더 쓰고
             // pintln 말고 print, printf 등을 써서 한줄로 출력
@@ -22,9 +22,7 @@ public class Star1 {
         }
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
         Star1 star = new Star1();
-        star.nightSky(str);
+        star.nightSky(10);
         }
     }

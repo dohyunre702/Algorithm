2c1181508227dbd701a85679035564f171bcc9d2
dohyu <dohyunre702@gmail.com>
2022-10-18 09:24:40 +0900

    square refactoring. repeat 메소드 사용

--- Likelion/Oct18/Square.java
+++ Likelion/Oct18/Square.java
@@ -3,21 +3,22 @@ package Likelion.Oct18;
 import java.util.Scanner;
 
 public class Square {
-    public void printsquare (int a) {
+    //repeat 메소드 사용 (모던 자바 스타일)
+    public void printSquare (int a) {
         for (int i = 0; i < a; i++) {
-            for (int j = 0; j <a; j++) {
-                System.out.print("*");
+                System.out.println("*".repeat(a));
             }
-            System.out.println();
         }
-
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
 
+        //바로 넣어주기
+        new Square().printSquare(num1);
+        /*
         Square instance1 = new Square();
-        instance1.printsquare(num1);
+        instance1.printSquare(num1);
+        */
     }
 }

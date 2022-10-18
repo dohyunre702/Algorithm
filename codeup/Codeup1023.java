ddaa0da8e968eac65f0fb43e2cf4a6ff3946a4cc
dohyu <dohyunre702@gmail.com>
2022-10-18 23:14:00 +0900

    구분자로 문자열 파싱, 이스케이프 문자 "\\."

--- codeup/Codeup1023.java
+++ codeup/Codeup1023.java
@@ -5,12 +5,28 @@ import java.util.Scanner;
 public class Codeup1023 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String number = sc.nextLine();
+        String[] seperateNum = number.split("\\.");
 
-        String[] seperateNum = number.split(".");
-
-        System.out.println(seperateNum[0]);
-        System.out.print(seperateNum[1]);
+        System.out.printf(seperateNum[0]);
+        System.out.println();
+        System.out.printf(seperateNum[1]);
 
     }
 }
+
+/*
+1. 구분자로 문자열 분리하기
+public class Main {
+	public static void main(String[] args) {
+		String str = "jhnyang@tistory@com";
+		String[] strAry = str.split("@");
+
+		for (String s : strAry)
+			System.out.println(s);
+	}
+}
+
+2. 이스케이프 문자 : 온점은 메타문자. "\\."으로 파싱
+ */

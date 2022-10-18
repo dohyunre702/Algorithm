8d76f630f3f0367839241f3920440bfd48048138
dohyu <dohyunre702@gmail.com>
2022-10-18 09:54:47 +0900

    시도1. 거꾸로 출력된 직각삼각형

---
+++ Likelion/Oct18/Codeup1859.java
@@ -0,0 +1,42 @@
+package Likelion.Oct18;
+
+import java.util.Scanner;
+
+public class Codeup1859 {
+    /* n = 4일 때,
+    f(4) = f(3) + 1
+         = f(2) + 1 + 1
+         = f(1) + 1 + 1 + 1
+    따라서,
+    f(1) = f(4) - 3
+    f(2) = f(4) - 2
+    f(3) = f(4) - 1
+    f(4) = print("*")
+    */
+
+    public void printRS(int a) {
+        if (a > 0) {
+            System.out.println("*".repeat(a));
+            printRS(a - 1);
+        }
+        else {
+            System.out.println("종료");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+
+        Codeup1859 recurStar = new Codeup1859();
+        recurStar.printRS(num1);
+    }
+}
+
+/*
+out
+****
+***
+**
+*
+ */

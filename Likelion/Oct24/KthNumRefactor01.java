b0a5c7d8ab9fb448a0e30df56a36b9625307b8ef
dohyu <dohyunre702@gmail.com>
2022-10-24 14:48:39 +0900

    K번째 수 3. for-each문

---
+++ Likelion/Oct24/KthNumRefactor01.java
@@ -0,0 +1,18 @@
+package Likelion.Oct24;
+
+import java.util.*;
+//K번쨰 수 리팩토링. For each문 사용
+
+public class KthNumRefactor01 {
+    public int[] solution(int[]arr, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int idx = 0;
+        for (int[] command : commands) {
+            int[] slicedArr = Arrays.copyOfRange(arr, command[0] - 1, command[1]);
+            Arrays.sort(slicedArr);
+            answer[idx++] = slicedArr[command[2] - 1];
+        }
+        return answer;
+    }
+
+}

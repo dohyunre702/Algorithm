05bf5ceb3c84d357fb620a0568f7cb5d2018b3e9
dohyu <dohyunre702@gmail.com>
2022-10-25 09:19:24 +0900

    해시함수 구현 2. 형변환 절차 생략 (char - int)

--- Likelion/relatedBasics/HashFunction.java
+++ Likelion/relatedBasics/HashFunction.java
@@ -4,10 +4,7 @@ public class HashFunction {
     public int hash(String key) {
         int asciiSum = 0;
         for(int i = 0; i <key.length(); i++) {
-            char c = key.charAt(i);
-            int ascii = c;
-            System.out.printf("%s %d\n", c, ascii);
-            asciiSum += ascii;
+            asciiSum = key.charAt(i);
         }
         System.out.println("asciiSum:" + asciiSum);
         return asciiSum % 90;

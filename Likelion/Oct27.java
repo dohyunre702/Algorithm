50707ba7acf039490277e0bfc3f821225eb270e2
dohyu <dohyunre702@gmail.com>
2022-10-27 09:24:34 +0900

    완주하지 못한 선수 - 중간점검

---
+++ Likelion/Oct27.java
@@ -0,0 +1,24 @@
+package Likelion;
+
+//프로그래머스 완주하지 못한 선수
+class Oct27 {
+    //해시 테이블에 participant 모두 넣기
+    //해시 테이블에 completion 모두 넣기
+    //value = 1인 이름 찾기
+
+    //해시 함수
+    public int hash(String key) {
+        int num = 0;
+        for (int i = 0; i < key.length(); i++) {
+            num += ;
+        }
+        return num % key.length();
+    }
+
+    public String solution(String[] participant, String[] completion) {
+
+
+            String answer = "";
+            return answer;
+        }
+    }

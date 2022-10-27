54156971e22213a9bf1e858408dc7c729a524487
dohyu <dohyunre702@gmail.com>
2022-10-27 11:00:07 +0900

    완주하지 못한 선수 - 로직2

--- Likelion/Oct27.java
+++ Likelion/Oct27.java
@@ -10,22 +10,30 @@ class Oct27 {
     //memo를 1바퀴 돌며 1인 값 찾기
     //1명인 key return
     //문제점 : 동명이인 처리 불가
-    //내가 몰랐던 거 : Map, HashMap 메서드 활용법
+
+    //내가 몰랐던 거 : Map, HashMap 메서드 활용 > 다시 확인하기
 
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> memo = new HashMap<>();
 
+        //1. 1차로 put 값 넣기
         for(int i = 0; i < participant.length; i++) {
             String key = participant[i];
-            memo.put(key, 1);
+            //바로 put을 할 수 없다.(해시 충돌)
+            if (!memo.containsKey(key)) { //값 없으면 0으로 초화
+                memo.put(key, 0);
+            }
+            //.get(key)는 key의 value 갖고 옴
+            memo.put(key, memo.get(key) + 1); //값 있으면 +1 count
         }
 
+        //2. completion 한 바퀴 돌려서
         for(int i = 0; i < completion.length; i++) {
             String key = completion[i];
-            memo.put(key, 0);
+            memo.put(key, memo.get(key) - 1); //값이 있으면 다시 -1 count
         }
 
-        for(String key : memo.keySet()) {
+        for(String key : memo.keySet()) { //key값 순회해서 1인 값 key return
             if (memo.get(key) == 1)
                 return key;
         }

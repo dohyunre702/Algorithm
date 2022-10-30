9a308fe0c6f766dab9f3dda48f71b020281c5e22
dohyu <dohyunre702@gmail.com>
2022-10-30 20:35:45 +0900

    파도반 수열 - for문

--- BOJ9461.java
+++ BOJ9461.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
-//파도반 수열
+//파도반 수열 - 통과 불가. 왜?
 public class BOJ9461 {
-    static long padoban(int x) {
-        if (x <= 3 ) {
-            return 1;
-        } else if (x <= 5) {
-            return 2;
-        } else {
-            return padoban(x - 2) + padoban(x - 3);
-        }
-    }
-    //피보나치 수 1
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        long[] memo = new long[101];
 
-        System.out.println(padoban(n));
+        //2. for문으로 해결
+        //if로 넣으면 i=1~5 값 할당이 안됨
+        memo[1] = memo[2] = memo[3] = 1;
+        memo[4] = memo[5] = 2;
+
+        for (int i = 6; i <= 100; i++) {
+            memo[i] = memo[i - 1] + memo[i - 5];
+        }
+
+        System.out.println(memo[n]);
+        }
     }
-}

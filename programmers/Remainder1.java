16a84565b69fe4e92c906c6fb90a367882d7c9ee
dohyu <dohyunre702@gmail.com>
2022-10-30 15:12:09 +0900

    알고리즘 10/30

---
+++ programmers/Remainder1.java
@@ -0,0 +1,15 @@
+package programmers;
+
+//나머지가 1이 되는 수 찾기
+public class Remainder1 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}

8a29dad47a2b9143adea5b55f17c27a18282d329
dohyu <dohyunre702@gmail.com>
2022-10-30 14:58:42 +0900

    알고리즘 10/30 - 알고리줌 수정

--- programmers/numbersX.java
+++ programmers/numbersX.java
@@ -1,12 +1,14 @@
 package programmers;
 
 //x만큼 간격이 있는 n개의 숫자
+//인덱스를 조절해 보았다.
 public class numbersX {
-        public long[] solution(long x, int n) {
-            long[] answer = new long[n];
-            for(int i = 0; i < n; i++){
-                answer[i] = x * (i + 1);
-            }
-            return answer;
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i-1] + x;
         }
+        return answer;
     }
+}

5bcda94c1633c24f6c9ca439052c978585a97647
dohyu <dohyunre702@gmail.com>
2022-10-31 09:44:08 +0900

    모의고사 - 생각2

--- Likelion/Oct31.java
+++ Likelion/Oct31.java
@@ -5,10 +5,23 @@ package Likelion;
 //길이 맞추기 > 몫만큼 곱해서 반복하고 나머지 더해주기
 //각 배열을 answers과 비교해 값이 같으면 count++
 //새로운 배열에 count의 최대값 넣기
+//리스트?
 
 public class Oct31 {
     public int[] solution(int[] arr) {
+        int[] result = new int[]{};
+        int[] array1 = {1,2,3,4,5};
+        int[] array2 = {2,1,2,3,2,4,2,5};
+        int[] array3 = {3,3,1,1,2,2,4,4,5,5};
 
+        int firstStudentAnswerCnt = 0;
+        System.out.println(firstStudentAnswerCnt);
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
     }
 
 }

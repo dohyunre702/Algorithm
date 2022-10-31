5f6688c0506d42af0b87e42a1b067a6e3a0aa79c
dohyu <dohyunre702@gmail.com>
2022-10-31 09:37:10 +0900

    모의고사 - 생각

---
+++ Likelion/Oct31.java
@@ -0,0 +1,14 @@
+package Likelion;
+//모의고사
+
+//정답과 비교하려면 먼저 배열 길이를 맞춰주어야 한다
+//길이 맞추기 > 몫만큼 곱해서 반복하고 나머지 더해주기
+//각 배열을 answers과 비교해 값이 같으면 count++
+//새로운 배열에 count의 최대값 넣기
+
+public class Oct31 {
+    public int[] solution(int[] arr) {
+
+    }
+
+}

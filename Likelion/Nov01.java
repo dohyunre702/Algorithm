6f5c4eecf0aeea7596077ce821f1126e8dd6ff22
dohyu <dohyunre702@gmail.com>
2022-11-01 09:36:46 +0900

    소수 알고리즘. 1번 단순 나눗셈

--- Likelion/Nov01.java
+++ Likelion/Nov01.java
@@ -1,25 +1,25 @@
 package Likelion;
 
 //13,17,19,23이 소수인지 판별하는 알고리즘 만들기
-//배열 1개 만든다./
 
 //1. 단순 나눗셈 : n % i = 0이 있으면 0
-//빈 배열을 만들고, n % i = 0이라면 [i] ++
-//[i] == 0인 인덱스의 개수 구하기
+
+class PrimeNum01 {
+    public boolean result(int arr[]) {
+        boolean answer = false;
+        for (int i = 2; i <= arr.length; i++) {
+            if (arr[i] % i != 0) {
+                answer = true;
+            }
+        }   return answer;
+    }
+}
 
 public class Nov01 {
-    public boolean primeNum(int a) {
-        for (int i = 0; i < a; i++) {
-            if (a % i == 0) {
-
-            }
-        }                 return true;
-    }
-
-    //배열로 구해보기
-    public boolean primeNum2(int[] arr) {
+    public static void main(String[] args) {
+        PrimeNum01 primenum = new PrimeNum01();
         int[] primeArr = {13, 17, 19, 23};
-
+        System.out.println(primenum.result(primeArr));
     }
 }
 

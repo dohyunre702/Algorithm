f8af0db2f79d8a39a6cbb9f9eddcc7abac4915be
dohyu <dohyunre702@gmail.com>
2022-11-01 10:47:26 +0900

    소수 알고리즘. 콜백 패턴 적용 1-2. 패턴 완성

--- Likelion/Nov01TemplateCallback.java
+++ Likelion/Nov01TemplateCallback.java
@@ -6,12 +6,6 @@ interface StatementStrategy {
 
 //템플릿 콜백 적용 예시
 public class Nov01TemplateCallback {
-    /* a<b는 어디로 가지?
-    boolean someOperation(int a, int b) {
-        return a < b;
-    }
-
-     */
     boolean isPrime(int num, StatementStrategy stmt) { //인터페이스 콜백
         for (int i =2; stmt.compare(i, num); i++) {
             if (num % i == 0) return false;
@@ -21,7 +15,12 @@ public class Nov01TemplateCallback {
 
     public static void main(String[] args) {
         Nov01TemplateCallback tcp = new Nov01TemplateCallback();
-        boolean r = tcp.isPrime(17);
+        boolean r = tcp.isPrime(17, new StatementStrategy() {//콜백
+            @Override
+            public boolean compare(int a, int b) {
+                return a <= b; //1번 방법
+            }
+        }); //이 부분을 람다 표현식으로 바꿀 예정
         System.out.println(r);
     }
 }

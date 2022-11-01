68ea64912d503626248ae7fb0cb2754158d83d2b
dohyu <dohyunre702@gmail.com>
2022-11-01 09:52:43 +0900

    소수 알고리즘. 콜백 위해 인터페이스 적용 중

--- Likelion/Nov01.java
+++ Likelion/Nov01.java
@@ -2,9 +2,30 @@ package Likelion;
 
 //13,17,19,23이 소수인지 판별하는 알고리즘 만들기
 
-//3번. 제곱근 -2
-class PrimeNum01 {
-    public boolean result(int arr[]) {
+//3번 template callback 적용 연습
+class PrimeNum01 implements PrimeNumGenerator{
+    @Override
+    public boolean result01(int arr[]) {
+        boolean answer = false;
+        for (int i = 2; i <= arr.length ; i++) {
+            if (arr[i] % i != 0) {
+                answer = true;
+            }
+        }   return answer;
+    }
+
+    @Override
+    public boolean result02(int arr[]) {
+        boolean answer = false;
+        for (int i = 2; i <= (int)arr.length/2 ; i++) {
+            if (arr[i] % i != 0) {
+                answer = true;
+            }
+        }   return answer;
+    }
+
+    @Override
+    public boolean result03(int arr[]) {
         boolean answer = false;
         for (int i = 2; i * i <= arr.length ; i++) {
             if (arr[i] % i != 0) {
---
+++ Likelion/PrimeNumGenerator.java
@@ -0,0 +1,7 @@
+package Likelion;
+
+public interface PrimeNumGenerator {
+    public boolean result01(int arr[]);
+    public boolean result02(int arr[]);
+    public boolean result03(int arr[]);
+}

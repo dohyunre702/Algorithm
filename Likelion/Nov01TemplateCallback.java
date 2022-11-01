36c75ac4e42c91e2b0dfc085e665e8e7ee1c59e7
dohyu <dohyunre702@gmail.com>
2022-11-01 10:50:14 +0900

    소수 알고리즘. 콜백 패턴 예시 2단계. 람다 표현식

--- Likelion/Nov01TemplateCallback.java
+++ Likelion/Nov01TemplateCallback.java
@@ -7,7 +7,7 @@ interface StatementStrategy {
 //템플릿 콜백 적용 예시
 public class Nov01TemplateCallback {
     boolean isPrime(int num, StatementStrategy stmt) { //인터페이스 콜백
-        for (int i =2; stmt.compare(i, num); i++) {
+        for (int i = 2; stmt.compare(i, num); i++) {
             if (num % i == 0) return false;
         }
         return true;
@@ -15,12 +15,9 @@ public class Nov01TemplateCallback {
 
     public static void main(String[] args) {
         Nov01TemplateCallback tcp = new Nov01TemplateCallback();
-        boolean r = tcp.isPrime(17, new StatementStrategy() {//콜백
-            @Override
-            public boolean compare(int a, int b) {
-                return a <= b; //1번 방법
-            }
-        }); //이 부분을 람다 표현식으로 바꿀 예정
-        System.out.println(r);
+        //람다
+        System.out.println(tcp.isPrime(13, (a, b) -> a < b));
+        System.out.println(tcp.isPrime(17, (a, b) -> a < b / 2));
+        System.out.println(tcp.isPrime(19, (a, b) -> a * a < b));
     }
 }

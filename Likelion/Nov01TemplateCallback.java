261a1c0934c425fba5625d242e415412e9e083a1
dohyu <dohyunre702@gmail.com>
2022-11-01 10:41:53 +0900

    소수 알고리즘. 콜백 패턴 적용 1단계- 메서드는 어디로?

--- Likelion/Nov01TemplateCallback.java
+++ Likelion/Nov01TemplateCallback.java
@@ -1,12 +1,19 @@
 package Likelion;
 
+interface StatementStrategy {
+    boolean compare(int a, int b);
+}
+
 //템플릿 콜백 적용 예시
 public class Nov01TemplateCallback {
+    /* a<b는 어디로 가지?
     boolean someOperation(int a, int b) {
         return a < b;
     }
-    boolean isPrime(int num) {
-        for (int i =2; someOperation(i, num); i++) { //메서드 콜백
+
+     */
+    boolean isPrime(int num, StatementStrategy stmt) { //인터페이스 콜백
+        for (int i =2; stmt.compare(i, num); i++) {
             if (num % i == 0) return false;
         }
         return true;

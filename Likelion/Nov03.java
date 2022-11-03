d3feabd6898119f1e190972e85a338b1f8cd3dae
dohyu <dohyunre702@gmail.com>
2022-11-03 10:32:47 +0900

    11/03 에라토스테네스의 체. 불리언 배열로 true인 원소 출력 - 클래스로 분리

--- Likelion/Nov03.java
+++ Likelion/Nov03.java
@@ -6,6 +6,18 @@ import java.util.List;
 
 //에라토스테네스의 체 배열 - checks를 참고해 true인 nums[i]만 출력
 public class Nov03 {
+    public static void printNums(int[] nums, boolean[] checks) {
+        int cnt = 0;
+        for (int i = 0; i < checks.length; i++) {
+            if(checks[i]) {
+                System.out.printf("%d|", nums[i]);
+                cnt++;
+            }
+        }
+        System.out.println();
+        System.out.println("size:" + cnt);
+    }
+
     public static void main(String[] args) {
         int N = 50;
         int[] nums = new int[N - 1]; //n-1만큼. 2~50 (49개)
@@ -18,11 +30,8 @@ public class Nov03 {
         int multipleOf = 2;
         for (int i = 2; i < nums.length; i += 2) checks[i] = false; //2배수 지우기
 
-        List<Integer> prime = new ArrayList<>();
-        for (int i = 0; i < checks.length; i++) {
-            if (checks[i]) prime.add(nums[i]);
-        }
-            System.out.println(prime);
-        }
+        Nov03 checkPrime = new Nov03();
+        checkPrime.printNums(nums, checks);
+    }
     }
 

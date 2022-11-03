aa8050e29b3781d5eb65870ea76bf6f1a7b00b5c
dohyu <dohyunre702@gmail.com>
2022-11-03 09:44:02 +0900

    11/03 실습 1 완료 - 정리

--- Likelion/Nov03.java
+++ Likelion/Nov03.java
@@ -2,7 +2,7 @@ package Likelion;
 
 //에라토스테네스의 체 배열 1단계
 //n = 100, 0인덱스 : 1씩 증가, 1인덱스 : 2배수씩 증가
-public class Nov03map {
+public class Nov03 {
     public static void main(String[] args) {
         int[] arr = new int[2];
         int i = 0;
@@ -10,8 +10,7 @@ public class Nov03map {
         while(i<=98) {
             arr[0] = i+2;
             arr[1] = (i+2)*2;
-            System.out.printf("[%d, %d]", arr[0], arr[1]);
-            System.out.println();
+            System.out.printf("[%d, %d]\n", arr[0], arr[1]);
             i++;
         }
     }

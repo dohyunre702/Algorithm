9bce97af255f1268a2d58e7aea8377ba31503695
dohyu <dohyunre702@gmail.com>
2022-11-03 09:37:53 +0900

    11/03 실습 1 완료 - 인덱스 조정

--- Likelion/Nov03map.java
+++ Likelion/Nov03map.java
@@ -5,11 +5,11 @@ package Likelion;
 public class Nov03map {
     public static void main(String[] args) {
         int[] arr = new int[2];
-        int i = 2;
+        int i = 0;
 
-        while(i<=100) {
-            arr[0] = i;
-            arr[1] = i*2;
+        while(i<=98) {
+            arr[0] = i+2;
+            arr[1] = (i+2)*2;
             System.out.printf("[%d, %d]", arr[0], arr[1]);
             System.out.println();
             i++;

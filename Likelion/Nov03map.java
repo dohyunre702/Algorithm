0099b43db73b93efa2e61bf61673bcb3da8d024d
dohyu <dohyunre702@gmail.com>
2022-11-03 09:25:14 +0900

    11/03 실습 1

---
+++ Likelion/Nov03map.java
@@ -0,0 +1,14 @@
+package Likelion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Nov03map {
+    public static void main(String[] args) {
+        Map<Integer, Integer> multipleof = new HashMap<>();
+
+        //n = 2에서 시작
+        //key에 n+1
+        //value에 n*2
+    }
+}

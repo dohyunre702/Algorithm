d222d20f2fe7e86caa9963b93e2721107fd49893
dohyu <dohyunre702@gmail.com>
2022-11-07 09:13:52 +0900

    11.07 하샤드 수

---
+++ Likelion/Nov07.java
@@ -0,0 +1,21 @@
+package Likelion;
+
+//하샤드 수
+// 몫 = tmp
+// 나머지 = 합에 더하기
+// 합 반환
+
+public class Nov07 {
+    static int solution(int x) {
+        int sum = 0;
+        int tmp = 0;
+
+        while (tmp > 0) {
+            int a = x % 10;
+            tmp = x / 10;
+
+            sum += a;
+        }
+        return sum;
+    }
+}

bca60a8b8f468faf1973c4aa8ac4aacd69843e2f
dohyu <dohyunre702@gmail.com>
2022-11-08 10:24:18 +0900

    11.08 이진탐색 진행 중

---
+++ Likelion/BinarySearch.java
@@ -0,0 +1,16 @@
+package Likelion;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        //중간값 찾기
+        int k = (int)nums.length/2;
+
+        //인덱스(시작점, 끝점) 옮기기
+
+
+        //같은지 비교하기
+
+    }
+}

1bd6c0a4dbfddc38d1d5eea28a8f23c9706e1625
dohyu <dohyunre702@gmail.com>
2022-11-09 09:58:52 +0900

    11.09 선택정렬 진행 중

--- Likelion/Nov08.java
+++ Likelion/Nov08.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 
 
 //codeup3001. 데이터 탐색
-//-1 출력. 왜??
 public class Nov08 {
     public int linearSearch(int[] arr, int a) {
-        int i = 0;
-
-        while(true) {
-            if (i == arr.length) {
-                return -1;
-            }
+        int number = 0;
+        for(int i = 0; i < arr.length; i++) {
             if (arr[i] == a) {
-                return arr[i];
+                number = i+1;
+                break;
+            } else {
+                number = -1;
             }
-            i++;
         }
+        return number;
     }
 
     public static void main(String[] args) {
@@ -30,12 +28,7 @@ public class Nov08 {
             numbers[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < n; i++) {
-            System.out.printf("%d, ", numbers[i]);
-        }
-
         Nov08 result = new Nov08();
         System.out.print(result.linearSearch(numbers, k));
-
     }
 }

20a7fc4acaa66fccdcd188cbf4eb3b3ba2d3df34
dohyu <dohyunre702@gmail.com>
2022-11-10 09:19:11 +0900

    11.10 같은 숫자는 싫어 진행중

---
+++ Likelion/Nov10.java
@@ -0,0 +1,22 @@
+package Likelion;
+
+import java.util.*;
+
+public class Nov10 {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        List<Integer> list1 = new ArrayList<>();
+
+        list1.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++) {
+                // 앞의 원소들과 비교해 값이 없으면
+                // 해당 원소 추가하기
+            }
+        }
+
+        //리스트 반환
+
+        return answer;
+    }
+}

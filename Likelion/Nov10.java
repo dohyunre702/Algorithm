0551e9712178d00887481c6fd2934508bc87ac32
dohyu <dohyunre702@gmail.com>
2022-11-10 09:41:21 +0900

    11.10 같은 숫자는 싫어 (스택X) 완료

--- Likelion/Nov10.java
+++ Likelion/Nov10.java
@@ -4,18 +4,23 @@ import java.util.*;
 
 //같은 숫자는 싫어
 public class Nov10 {
-    public Object[] solution(int[] arr) {
+    public int[] solution(int[] arr) {
         List<Integer> list1 = new ArrayList<>();
 
         list1.add(arr[0]);
+
+        //배열의 idx[i]값을 리스트의 마지막 값과 비교해 같은 숫자가 없으면 add
         for (int i = 1; i < arr.length; i++) {
-            for (int j = 2; j < arr[i]; j++) {
-                if (arr[i] != arr[j]) {
-                    list1.add(arr[i]);
-                }
+            if (list1.get(list1.size()-1) != arr[i])
+                list1.add(arr[i]);
             }
+
+        int[] answer = new int[list1.size()];
+
+        //integer list를 int array로 변경
+        for (int i = 0; i < list1.size(); i++) {
+            answer[i] = list1.get(i).intValue();
         }
-        Object[] answer = list1.toArray();
 
         return answer;
     }

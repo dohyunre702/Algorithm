054b9610e629a9e61d929378b9a2eeb63d95a101
dohyu <dohyunre702@gmail.com>
2022-11-11 09:31:02 +0900

    11.11 10진수 2진수로 변환하기

---
+++ Likelion/Nov/Prac.java
@@ -0,0 +1,13 @@
+package Likelion.Nov;
+
+import Likelion.Nov11;
+
+//10진수 2진수로 변환
+public class Prac {
+    public static void main(String[] args) {
+        String a = Integer.toBinaryString(28);
+        System.out.println(a);
+        //11100
+
+        }
+    }

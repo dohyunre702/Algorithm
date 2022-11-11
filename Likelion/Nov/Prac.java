63b3c06d58ef0109d877a9cbce1ecbc090a8b814
dohyu <dohyunre702@gmail.com>
2022-11-11 09:45:45 +0900

    11.11 10진수 2진수로 변환하기 1. 메서드 2. 나누기, String-int 형변환

--- Likelion/Nov/Prac.java
+++ Likelion/Nov/Prac.java
@@ -2,12 +2,27 @@ package Likelion.Nov;
 
 import Likelion.Nov11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //10진수 2진수로 변환
 public class Prac {
     public static void main(String[] args) {
+
+        //1. 메서드
         String a = Integer.toBinaryString(28);
         System.out.println(a);
         //11100
+
+        //2. 나누기
+        String answer = "";
+        int b = 28;
+        while(b>0) {
+            answer += b % 2;
+            b = b/2;
+            //String + int = String
+        }
+        System.out.println(answer);
 
         }
     }

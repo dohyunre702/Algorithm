8721959151f4ceae96b1abed3e359a9acc3840ba
dohyu <dohyunre702@gmail.com>
2022-11-15 09:29:51 +0900

    11.15 퀵 정렬

---
+++ Likelion/Nov15.java
@@ -0,0 +1,20 @@
+package Likelion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//퀵 정렬
+public class Nov15 {
+    public static void main(String[] args) {
+        int[] arr = {20, 18, 5, 19, 5, 25, 40, 50};
+
+
+        //기준값 뽑는 로직 구현
+
+
+        //기준값 기준으로 왼쪽 오른쪽 나누어 담는 로직 구현
+        List<Integer> list1 = new ArrayList<>();
+
+    }
+
+}

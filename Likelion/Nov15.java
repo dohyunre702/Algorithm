f17843f9f40f5e809eed68d927ebe0de276ad59c
dohyu <dohyunre702@gmail.com>
2022-11-15 10:11:57 +0900

    11.15 퀵정렬 2단계까지

--- Likelion/Nov15.java
+++ Likelion/Nov15.java
@@ -10,10 +10,21 @@ public class Nov15 {
 
 
         //기준값 뽑는 로직 구현
+        int pivot = arr[arr.length/2]; //5
+        System.out.println(pivot);
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if(pivot > arr[i]) left.add(arr[i]);
+            else if(pivot < arr[i]) right.add(arr[i]);
+        }
+        System.out.println(left);
+        System.out.println(right);
 
 
         //기준값 기준으로 왼쪽 오른쪽 나누어 담는 로직 구현
-        List<Integer> list1 = new ArrayList<>();
+        return sort(left) + pivot + sort(right);
 
     }
 

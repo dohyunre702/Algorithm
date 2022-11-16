e468a428093a5ed0fe5542e930107f9d86335d88
dohyu <dohyunre702@gmail.com>
2022-11-16 11:08:36 +0900

    콜라츠 해결 1. while 조건 수정 2. int > long

--- programmers/Collatz.java
+++ programmers/Collatz.java
@@ -4,26 +4,19 @@ import java.util.Scanner;
 
 //콜라츠 추측(collatz conjecture)
 public class Collatz {
-    public int solution(int num) {
+    public int solution(long num) {
         int answer = 0;
-        int cnt = 0;
-        if (num == 1) answer = 0;
-        else {
-            int n = num;
-            while (n == 1) {
-                if (n % 2 == 0) {
-                    n = n / 2;
-                    cnt++;
-                } else if (n % 2 != 0) {
-                    n = n * 3 + 1;
-                    cnt++;
-                }
-            return cnt;
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num * 3 + 1;
             }
-            answer = cnt;
-        }
-        if (answer > 500) {
-            answer = -1;
+            answer++;
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
         }
         return answer;
     }

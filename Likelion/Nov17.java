730f75513d6b0632edfd1db9117887d2d7f8ca38
dohyu <dohyunre702@gmail.com>
2022-11-17 09:38:33 +0900

    퀵 정렬 - 재귀 사용하지 않음

---
+++ Likelion/Nov17.java
@@ -0,0 +1,26 @@
+package Likelion;
+
+import java.util.Arrays;
+
+public class Nov17 {
+    public int[] sort(int[] arr,int startIdx, int endIdx) {
+        int pivot = arr[arr.length/2];
+        System.out.println(pivot);
+        int leftIdx = 0;
+        int rightIdx = arr.length-1;
+
+        //아래반복
+        while(arr[leftIdx]<pivot)leftIdx +=1;
+        while(arr[rightIdx]>pivot)rightIdx-=-1;
+
+        int temp=arr[leftIdx];
+        arr[leftIdx] = arr[rightIdx];
+        arr[rightIdx] = temp;
+        leftIdx +=1;
+        rightIdx -=1;
+        System.out.printf("4:%d 7:%d\n", arr[4], arr[7]); // 25, 40
+        System.out.printf("leftIdx:%d rightIdx:%d\n", leftIdx, rightIdx); // 5, 6
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+}

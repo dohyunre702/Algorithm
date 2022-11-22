7ea4cb0589c039e3325d25fa9fc8b60ddfe3b163
dohyu <dohyunre702@gmail.com>
2022-11-22 10:49:37 +0900

    재귀: array에 있는 모든 값 더하기 - answer 매개변수 제거(공간복잡도 감소)

--- Likelion/Nov/Nov22_2.java
+++ Likelion/Nov/Nov22_2.java
@@ -3,14 +3,14 @@ package Likelion.Nov;
 //array에 있는 모든 값 더하기(재귀)
 public class Nov22_2 {
 
-    static int sum(int[] arr, int answer, int idx) {
-        if(arr.length == idx) return answer;
-        return sum(arr, answer + arr[idx], idx + 1);
+    static int sum(int[] arr, int idx) {
+        if(arr.length == idx) return 0;
+        return sum(arr, idx + 1);
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{7, 3, 2, 9};
-        int r = sum(arr, 0, 0);
+        int r = sum(arr, 0);
         System.out.println(r);
     }
 }

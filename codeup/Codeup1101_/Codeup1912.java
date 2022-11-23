e75568d8be3087932966c825b46f7f1c9d2a93e0
dohyu <dohyunre702@gmail.com>
2022-11-23 09:29:28 +0900

    재귀: 피보나치 수열

---
+++ codeup/Codeup1101_/Codeup1912.java
@@ -0,0 +1,23 @@
+package codeup.Codeup1101_;
+
+import java.util.Scanner;
+
+public class Codeup1912 {
+    public static int factorial(int n) {
+        int a = n;
+        if(n==1) return 1;
+        else {
+            a = n * factorial(n-1);
+            return a;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        Codeup1912 result = new Codeup1912();
+        System.out.println(result.factorial(num));
+    }
+}
+

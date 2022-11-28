91273da82afce5a9d23dee7c53eaed48b2058a3a
dohyu <dohyunre702@gmail.com>
2022-11-28 09:45:35 +0900

    알파벳 출력: a-z, 재귀

--- Likelion/Nov28.java
+++ Likelion/Nov28.java
@@ -1,7 +1,15 @@
 package Likelion;
 
 public class Nov28 {
-    public static void main(String[] args) {
+    public static void printAtoZ(char c){
+        if (c > 'Z') return;
+        else {
+            System.out.println(c);
+            printAtoZ((char) (c+1));
+        }
+    }
 
+    public static void main(String[] args) {
+        printAtoZ('A');
     }
 }

ca1415deabf9c11a60dd4aca7bdd48d2063bf150
dohyu <dohyunre702@gmail.com>
2022-11-30 10:30:48 +0900

    [update] lv.1 문자열 내 p와 y의 개수 - toLowerCase() 사용

--- programmers/lv1/PYpyinString.java
+++ programmers/lv1/PYpyinString.java
@@ -3,30 +3,19 @@ package programmers.lv1;
 //문자열 내 p와 y의 개수
 public class PYpyinString {
     boolean solution(String s) {
-        char[] arr = new char[s.length()];
-        boolean answer = true;
+        s = s.toLowerCase();
+
         int cntp = 0;
         int cnty = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            arr[i] = s.charAt(i);
+            if (s.charAt(i) == 'p') cntp += 1;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 'p' || arr[i] == 'P')
-                cntp += 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'y') cnty += 1;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 'y' || arr[i] == 'Y')
-                cnty += 1;
-        }
-
-        if (cntp == cnty) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-        return answer;
+        return cntp == cnty;
     }
 }

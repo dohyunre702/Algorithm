e6ef0602a164fb6a9cb08dd2a86665fb5a78f082
dohyu <dohyunre702@gmail.com>
2022-12-01 11:43:03 +0900

    [add] lv1. 기사단원의 무기 완료!

--- programmers/lv1/WeaponOfKnight.java
+++ programmers/lv1/WeaponOfKnight.java
@@ -1,34 +1,32 @@
 package programmers.lv1;
 //기사단원의 무기
 /*
-1. 각 수의 약수의 개수부터 구한 다음 배열에 담는다.
-2. for문 > 돌려서 limit과 비교
- 1) 약수 개수 > limit : power 대입
- 2) 약수 개수 <= limit : [i] 대입. answer++
+1. number의 제곱근으로 약수를 구하는 메서드 1개 만들기
+2. for문으로 1부터 num까지 약수의 개수를 더하기
+**와우 성능 차이가 엄청 나긴 하는구나 1번 문제만 해도 약 40배이다(127ms -> 3.42ms)
 */
 import java.util.Scanner;
 public class WeaponOfKnight {
     public static int solution(int number, int limit, int power) {
-        int answer = 0;
-        int[] arr = new int[number];
-
-        for(int i = 1; i <= number; i++) {
-            int tmp = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    tmp++;
-                }
-                arr[i-1] = tmp;
-            }
+    int answer = 0;
+    for (int i = 1; i <= number; i++) {
+        answer += numberofPrimes(i, limit, power);
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= limit) answer = answer + arr[i];
-            else answer = answer + power;
-        }
-        return answer;
+    return answer;
     }
 
+    public static int numberofPrimes(int number, int limit, int power) {
+        int cnt = 0;
+        //약수의 개수. 제곱근으로 구한 뒤 제곱수+1, 나머지 +2(순서쌍)
+        for (int i = 1; i * i <= number; i++) {
+            if (number % i == 0) {
+                if (number / i == i) cnt++;
+                else cnt += 2;
+            }
+        }
+        if (cnt > limit) return power;
+        return cnt;
+    }
     //test
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -37,7 +35,6 @@ public class WeaponOfKnight {
         int pow = sc.nextInt();
 
         WeaponOfKnight result = new WeaponOfKnight();
-        //solution(num, lim, pow);
         System.out.println(solution(num, lim, pow));
     }
 }

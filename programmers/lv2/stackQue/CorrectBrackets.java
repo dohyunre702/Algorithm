a34346c3aab2f983c84daefdd5c3d54266d1b087
dohyu <dohyunre702@gmail.com>
2022-12-02 10:45:10 +0900

    [update] lv2. 올바른 괄호 (실패) - 스택, .charAt() 활용 : 5, 11, 17 실패

--- programmers/lv2/stackQue/CorrectBrackets.java
+++ programmers/lv2/stackQue/CorrectBrackets.java
@@ -1,19 +1,25 @@
 package programmers.lv2.stackQue;
 
+import java.util.Stack;
+
 //올바른 괄호
 class CorrectBrackets {
     boolean solution(String s) {
         boolean answer = true;
-
         //1. 처음에 (로 시작해야 한다
         //2. 마지막에 )로 끝마쳐야 한다
 
-        //문자열을 배열로 나누고, 첫 번째 인덱스 값이 '(' and 마지막 인덱스 값이 ')' 면 true
-        char[] charArr = s.toCharArray();
-        if (charArr[0] == '(' && charArr[s.length()-1] == ')')
-            answer = true;
-        else answer = false;
-
+        //stack 사용
+        Stack<Character> stk = new Stack<>();
+        stk.push(s.charAt(0));
+        if (stk.peek() != '(') answer = false;
+        else {
+            for (int i = 1; i < s.length(); i++) {
+                stk.push(s.charAt(i));
+            }
+            if (stk.peek() != ')') answer = false;
+            else answer = true;
+        }
         return answer;
     }
 }

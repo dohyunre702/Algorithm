f769095b10de354a0e5a074d494a4b225f82458d
dohyu <dohyunre702@gmail.com>
2022-12-02 11:29:34 +0900

    [update] lv2. 올바른 괄호 (실패) - toCharArray 사용하지만 여전히 5, 11은 틀린다. 왜? - 효율성은 모두 통과

--- programmers/lv2/stackQue/CorrectBrackets.java
+++ programmers/lv2/stackQue/CorrectBrackets.java
@@ -1,25 +1,40 @@
 package programmers.lv2.stackQue;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 //올바른 괄호
 class CorrectBrackets {
-    boolean solution(String s) {
+    static boolean solution(String s) {
         boolean answer = true;
         //1. 처음에 (로 시작해야 한다
         //2. 마지막에 )로 끝마쳐야 한다
+        //3. (와 )의 수가 같아야 한다
 
-        //stack 사용
-        Stack<Character> stk = new Stack<>();
-        stk.push(s.charAt(0));
-        if (stk.peek() != '(') answer = false;
-        else {
-            for (int i = 1; i < s.length(); i++) {
-                stk.push(s.charAt(i));
-            }
-            if (stk.peek() != ')') answer = false;
-            else answer = true;
+        int a = 0; int b = 0;
+
+        char[] charArr = s.toCharArray();
+
+        //() 3번 조건 체크하기 위한 반복문
+        for (int i = 0; i < charArr.length; i++) {
+            if (charArr[i] == '(') a++;
+            else b++;
         }
+
+        //조건 체크
+        if (a == b) {
+            if (charArr[0] == '(' && charArr[charArr.length - 1] == ')')
+                answer = true;
+            else answer = false;
+        } else answer = false;
         return answer;
+    }
+
+    //test
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String example = sc.next();
+
+        System.out.println(solution(example));
     }
 }

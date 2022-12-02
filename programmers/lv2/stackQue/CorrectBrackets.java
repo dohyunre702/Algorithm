ccf5b710c1daa3f1113bd1e537c23d996f4e2db0
dohyu <dohyunre702@gmail.com>
2022-12-02 13:16:59 +0900

    [update] lv2. 올바른 괄호 (힌트 얻어 성공) - stack 사용하지 않고 풀이

--- programmers/lv2/stackQue/CorrectBrackets.java
+++ programmers/lv2/stackQue/CorrectBrackets.java
@@ -1,27 +1,18 @@
 package programmers.lv2.stackQue;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 //올바른 괄호. for 내 조건 풀이
 class CorrectBrackets {
     static boolean solution(String s) {
-        boolean answer = true;
-        //반례: ()))((() ㅏㅏ 이래서 스택을 써야되는구나
-        //0. 가장 처음에는 ( 가 추가되어야 함
-        //1. 2~n-1번째 괄호는 이전 괄호와 다를 때만 추가.
-        //2. 마지막 괄호가 )이어야 함
-
-        Stack<Character> stk = new Stack<>();
+        boolean answer = false;
+        int count = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') //현재 ( 가 들어갈 자리면 스택에 넣기
-                stk.push(s.charAt(i));
-            else {
-                if (stk.isEmpty()) return false; //현재 )가 들어갈 자리인데 스택 비어있을 때
-                else stk.pop(); //현재 )가 들어갈 자리인데 스택에 ( 가 있는 경우 pop
-            }
+            if (s.charAt(i) == '(') count++;
+            if (s.charAt(i) == ')') count--;
+            if (count < 0) break;
         }
-        answer = (stk.isEmpty()) ? true : false;
+        if (count == 0) answer = true;
         return answer;
     }
 

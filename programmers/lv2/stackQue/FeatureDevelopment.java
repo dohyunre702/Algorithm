9ffb46080145dd3a0739f1b411f9808998856b58
dohyu <dohyunre702@gmail.com>
2022-12-04 12:06:12 +0900

    [add] lv2. 기능개발 - 구현. 로직 일부 수정 (35분) - [1,3,2] 에서 [1,3] 까지만 출력되는 중

--- programmers/lv2/stackQue/FeatureDevelopment.java
+++ programmers/lv2/stackQue/FeatureDevelopment.java
@@ -1,27 +1,56 @@
 package programmers.lv2.stackQue;
 
-//기능 개발
-/* 문제 분석 + 접근 방법 생각 25분
-1. 100 - progresses 의 각 인덱스값 : 몇일 걸리는지
-2. int a 선언 (= 스택의 첫 번째 값)
-3. 스택이 비어 있으면 첫 번째 원소 넣기
-4. 첫 번째 원소와 다음 원소 값 비교 [i], [i+1]
-  1) if [i] >= [i+1] : [i+1] 삽입
-  2) else if [i] < [i+1]
-    - 맨 처음 들어간 스택 값(int a) 반환해 answer에 넣기
-    - 스택 비우기
-    - [i+1] 삽입, int a에 해당 값 초기화
- */
-public class FeatureDevelopment {
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
-    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+//기능 개발
+public class FeatureDevelopment {
+    public static void solution(int[] progresses, int[] speeds) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
+        int cnt = 0; //
+
+        //몇 일 걸리는지
+        for (int i = 0; i < progresses.length; i++) {
+            progresses[i] = (int) Math.ceil((100-progresses[i]) / speeds[i]);
+        }
+
+        int j = 0;
+        //스택이 비어 있으면 원소 넣기
+        while (j < progresses.length-1) {
+            if (stk.isEmpty()) {
+                stk.add(progresses[j]);
+                cnt++;
+            }
+
+            //if [i] >= [i+1] : [i+1] 삽입, cnt++
+            if (stk.peek() >= progresses[j+1]) {
+                stk.add(progresses[j+1]);
+                cnt++;
+            } else { //cnt 반환해 answer에 넣기, 스택 비우고 cnt 초기화
+                ans.add(cnt);
+                cnt = 0;
+                stk.clear();
+            }
+            j++;
+        }
+
+        int[] answer = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
+
+        for (int i = 0; i < answer.length; i++) {
+            System.out.printf("%d, ", answer[i]);
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr1 = {95, 90, 99, 99, 80, 99};
         int[] arr2 = {1, 1, 1, 1, 1, 1};
-        }
+
+        FeatureDevelopment.solution(arr1, arr2);
 
     }
+ }

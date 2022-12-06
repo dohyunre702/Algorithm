342aaf0a3b19953a96c5ec9221851107e5f3fce9
dohyu <dohyunre702@gmail.com>
2022-12-06 13:43:59 +0900

    [add] 선택정렬 완료

--- codeup/sort/Codeup1442.java
+++ codeup/sort/Codeup1442.java
@@ -1,22 +1,36 @@
 package codeup.sort;
 
+import java.util.Scanner;
 
 public class Codeup1442 {
 
     //선택 정렬
     static void selectionSort(int[] arr) {
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[i]) swap(arr, i, j);
+            }
+        }
 
-        for(int i = 1; i < arr.length; i++) {
-            int tmp = arr[0];
-            if (arr[i] < tmp) tmp = arr[i];
-            arr[i-1] = tmp;
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
     }
 
-    public static void main(String[] args) {
-        //주어지는 숫자를 중복이 없는 배열로 변환해 입력해야 한다.
-        int[] exampleArr = {};
+    //swap 함수
+    static void swap(int[] arr, int i, int j) {
+        int k = arr[i];
+        arr[i] = arr[j];
+        arr[j] = k;
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] exampleArr = new int[num];
+        for (int i=0; i<exampleArr.length; i++) {
+            exampleArr[i] = sc.nextInt();
+        }
         selectionSort(exampleArr);
     }
 }

612f330f611cfbf8c2c4e18cfb5f14a90275f17e
dohyu <dohyunre702@gmail.com>
2022-12-06 14:15:39 +0900

    [add] 버블정렬 완료

--- codeup/sort/Codeup1441.java
+++ codeup/sort/Codeup1441.java
@@ -6,34 +6,30 @@ public class Codeup1441 {
 
     //버블정렬
     static void bubbleSort(int[] arr) {
-        int tmp = 0;
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    tmp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = tmp;
-                }
-            }
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = arr.length-1; j > i; j--)
+                if(arr[j-1] > arr[j]) swap(arr, j-1, j);
         }
 
-        //출력
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
+    //swap 함수
+    static void swap(int[] arr, int i, int j) {
+        int k = arr[i];
+        arr[i] = arr[j];
+        arr[j] = k;
+    }
+
     public static void main(String[] args) {
-
-        //주어지는 숫자를 중복이 없는 배열로 변환해 입력해야 한다. 아니 난데없이 여기서 막히네..
-        /*
-        - 숫자는 enter을 기준으로 입력됨 (몇 개가 입력되는지는 알 수 없음)
-        - 입력값을 리스트에 할당
-        - 리스트를 hashset으로 변환
-        */
-
-        int[] exampleArr = {};
-
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] exampleArr = new int[num];
+        for (int i=0; i<exampleArr.length; i++) {
+            exampleArr[i] = sc.nextInt();
+        }
         bubbleSort(exampleArr);
     }
 }

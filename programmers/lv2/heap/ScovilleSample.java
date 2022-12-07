68a31ed0780faa3baccf99a82afcdc13a86ba15a
dohyu <dohyunre702@gmail.com>
2022-12-07 10:44:06 +0900

    [update] 더 맵게 풀이 완

--- programmers/lv2/heap/ScovilleSample.java
+++ programmers/lv2/heap/ScovilleSample.java
@@ -7,7 +7,7 @@ while(pq.peek() < k) 동안 poll, 섞기, 섞은 것 pq에 넣기 + answer++
 
 import java.util.PriorityQueue;
 
-public class Hotter {
+public class ScovilleSample {
     public int solution(int[] scoville, int K) {
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
@@ -17,12 +17,14 @@ public class Hotter {
         }
         //섞기
         int answer = 0;
-        while(pq.peek() < K) {
-            int mix = pq.poll() + (pq.poll() * 2);
-            pq.add(mix);
+        while(pq.size() >=2 && pq.peek() < K) {
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a+ (b*2));
             answer++;
         }
 
+        if(pq.peek() < K) return -1;
         return answer;
     }
 }

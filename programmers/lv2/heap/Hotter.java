add250da00d5226bd0946a7528c18a46b8b29f75
dohyu <dohyunre702@gmail.com>
2022-12-07 09:55:31 +0900

    [add] 더 맵게 풀이중 - 로직 개발

--- programmers/lv2/heap/Hotter.java
+++ programmers/lv2/heap/Hotter.java
@@ -5,9 +5,21 @@ package programmers.lv2.heap;
 while(pq.peek() < k) 동안 poll, 섞기, 섞은 것 pq에 넣기 + answer++
  */
 
+import java.util.PriorityQueue;
+
 public class Hotter {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            pq.offer(scoville[i]);
+        }
+
+        //while
+        while(pq.peek() < K) {
+
+        }
         return answer;
     }
 }

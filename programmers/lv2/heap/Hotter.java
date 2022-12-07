2fc1a13fda8f503d247bce7975cdc9392ff200c3
dohyu <dohyunre702@gmail.com>
2022-12-07 09:53:25 +0900

    [add] 더 맵게 풀이중 - 로직 개발

---
+++ programmers/lv2/heap/Hotter.java
@@ -0,0 +1,13 @@
+package programmers.lv2.heap;
+
+//lv2. 더 맵게
+/*
+while(pq.peek() < k) 동안 poll, 섞기, 섞은 것 pq에 넣기 + answer++
+ */
+
+public class Hotter {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+}
